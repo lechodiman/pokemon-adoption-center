41135b6dda10da6e0b2e41ce2085985e8b7c8da2
Luis Chodiman <lechodiman@uc.cl>
2024-05-28 21:34:41 -0400

    fix: increase timeout for onAdoptionRequestCreated

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,11 +1,11 @@
 import * as cors from 'cors';
 import * as express from 'express';
-import { firestore } from 'firebase-functions';
+import { runWith } from 'firebase-functions';
 import { onRequest } from 'firebase-functions/v2/https';
+import * as path from 'path';
 import adoptionRequestsRouter from './api/adoption-requests/router';
 import pokemonRouter from './api/pokemon/router';
 import { AdoptionRequestsService } from './services/adoption-requests-service';
-import * as path from 'path';
 
 const app = express();
 
@@ -18,8 +18,8 @@ app.use(adoptionRequestsRouter);
 
 export const api = onRequest(app);
 
-export const onAdoptionRequestCreated = firestore
-  .document('adoptionRequests/{adoptionRequestID}')
+export const onAdoptionRequestCreated = runWith({ timeoutSeconds: 300 })
+  .firestore.document('adoptionRequests/{adoptionRequestID}')
   .onCreate(async (snapshot, context) => {
     const adoptionRequestId = context.params.adoptionRequestID;
     const pokemonID = snapshot.data().pokemonID;
